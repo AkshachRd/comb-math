56b9692112ce77afeafdad4a2ac0c7e4961231ce
Daniil <dr.chashka@gmail.com>
2021-01-30 09:28:39 +0300

    Сделано lw1_1

---
+++ lw1_1/lw1_1/lw1_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <array>
+#include <algorithm>
+#include <iterator>
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+	std::array<size_t, 20> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+	auto start = std::chrono::steady_clock::now();
+	std::sort(v.begin(), v.end());
+	do
+	{
+		//copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+		//std::cout << std::endl;
+	} while (std::next_permutation(v.begin(), v.end()));
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+	return 0;
+}
