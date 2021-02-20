4c45eeec09c8da305dbfc32f496aba8f555b3d70
Daniil <dr.chashka@gmail.com>
2021-02-20 03:20:06 +0300

    Исправлена ошибка в LW1_6

--- lw1_6/lw1_6/lw1_6.cpp
+++ lw1_6/lw1_6/lw1_6.cpp
@@ -35,4 +35,5 @@ int main()
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+    return 0;
 }
