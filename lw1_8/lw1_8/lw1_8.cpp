db3bf3f9a9f65782abc93eb8ed56cd65e48ae131
Daniil <dr.chashka@gmail.com>
2021-02-27 05:52:38 +0300

    Разработана программа LW1_8

--- lw1_8/lw1_8/lw1_8.cpp
+++ lw1_8/lw1_8/lw1_8.cpp
@@ -1,34 +1,85 @@
 ﻿#include <iostream>
 #include <optional>
+#include <algorithm>
 #include <vector>
-#include <utility>
 
 struct Backpack
 {
     int n;
-    int T;
-    int S;
-    std::vector<std::pair<int, int>> things;
+    int t;
+    int s;
+    std::vector<int> w;
+    std::vector<int> c;
 };
 
 Backpack ReadInput()
 {
     Backpack b;
-    std::cin >> b.n >> b.T >> b.S;
-    b.things.resize(b.n);
+    std::cin >> b.n >> b.t >> b.s;
+    b.w.resize(b.n);
+    b.c.resize(b.n);
     for (int i = 0; i < b.n; ++i)
     {
         int first, second;
         std::cin >> first >> second;
-        b.things[i] = std::make_pair(first, second);
+        b.w[i] = first;
+        b.c[i] = second;
     }
     return b;
 }
 
+void GenerateAllSubsetsOfASet(std::vector<int>& vec, int n)
+{
+    while (vec[n] != 1)
+    {
+        //std::reverse_copy(vec.begin(), vec.end() - 1, std::ostream_iterator<int>(std::cout, " "));
+        int i = 0;
+        while (vec[i] == 1)
+        {
+            vec[i] = 0;
+            ++i;
+        }
+        vec[i] = 1;
+    }
+}
+
 int main()
 {
-    Backpack j;
-    j = ReadInput();
-    std::cout << j.n << "#" << j.T << "#" << j.S;
+    Backpack b = ReadInput();
+
+    std::vector<int> vec;
+    vec.resize(b.n + 1, 0);
+    int sumW = 0, sumC = 0;
+    while (vec[b.n] != 1)
+    {
+        sumW = 0;
+        sumC = 0;
+        for (int i = 0; i < b.n; ++i)
+        {
+            if (vec[i] == 1)
+            {
+                sumW += b.w[i];
+                sumC += b.c[i];
+            }
+        }
+        if ((sumW > 0 && sumW <= b.t) && (sumC > 0 && sumC >= b.s))
+        {
+            break;
+        }
+        int i = 0;
+        while (vec[i] == 1)
+        {
+            vec[i] = 0;
+            ++i;
+        }
+        vec[i] = 1;
+    }
+
+    std::cout << sumW << std::endl;
+    for (int i = 0; i < b.n; ++i)
+    {
+        std::cout << vec[i];
+    }
+
     return 0;
 }
